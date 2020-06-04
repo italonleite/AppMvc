e90c67730144d8c440eaddf13150adfe20542bd8
Italo Nunes Leite <italonleite@gmail.com>
2020-06-03 23:40:01 -0300

    - Adicionando Interface IRepository Genérica, para comunicar com acesso a DADOS

---
+++ src/Business/Interfaces/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Interfaces
+{
+    public interface IRepository<TEntity> : IDisposable where TEntity : Entity
+    {
+        Task Adicionar(TEntity entity);
+        Task<TEntity> ObterPorId(Guid id);
+        Task<List<TEntity>> ObterTodos();
+        Task Atualizar(TEntity entity);
+        Task Remover(Guid id);
+        Task<int> SaveChanges();
+    }
+}
