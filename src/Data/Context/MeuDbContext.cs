dca27fa031a3e2e3b37e15fb0af68a1ee026bd15
Italo Nunes Leite <italonleite@gmail.com>
2020-06-04 00:09:28 -0300

    - DbContext criado.

---
+++ src/Data/Context/MeuDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Business.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Data.Context
+{
+    public class MeuDbContext : DbContext
+    {
+        public MeuDbContext(DbContextOptions<MeuDbContext> options)
+       : base(options)
+        { }
+
+        public DbSet<Paciente> Pacientes { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // anteriormente deveria fazer o Map de cada classe mapeada. 
+            // dessa forma com Assembly, agora é feito todos em uma única vez só.
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(MeuDbContext).Assembly);
+
+            //impedir que a exclusão da classe mãe leve os filhos juntos (cascate)
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys())) relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
+
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
