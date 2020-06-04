bb25604a80c70401acdc5d1097e300bf758fcfb1
Italo Nunes Leite <italonleite@gmail.com>
2020-06-04 00:01:42 -0300

    - Interface Paciente e Endereço com seus respectivos métodos especializados. - EnderecoPorId e PacienteEndereco

---
+++ src/Business/Interfaces/IEnderecoRepository.cs
@@ -0,0 +1,13 @@
+﻿using Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Interfaces
+{
+    public interface IEnderecoRepository : IRepository<Endereco>
+    {
+        Task<Endereco> ObterEnderecoPorPaciente(Guid pacienteId);
+    }
+}
---
+++ src/Business/Interfaces/IPacienteRepository.cs
@@ -0,0 +1,13 @@
+﻿using Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Interfaces
+{
+    public interface IPacienteRepository : IRepository<Paciente>
+    {
+        Task<Paciente> ObterPacienteEndereco(Guid id);
+    }
+}
