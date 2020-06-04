7b557b43b5c1abb2feca10d5325a38c0414a62ac
Italo Nunes Leite <italonleite@gmail.com>
2020-06-03 23:34:22 -0300

    - Classes da Model criada.

---
+++ src/Business/Models/Paciente.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Models
+{
+    public class Paciente : Entity
+    {
+        public string Nome { get; set; }
+        public DateTime Nascimento { get; set; }
+        public int Idade { get; set; }
+        public Sexo Sexo { get; set; }
+        public string Cpf { get; set; }
+        public string TitularCpf { get; set; }
+        public string IndicadoPor { get; set; }
+        public Endereco Endereco { get; set; }
+        public DateTime DataCadastro { get; set; }
+    }
+}
---
+++ src/Business/Models/Sexo.cs
@@ -0,0 +1,8 @@
+﻿namespace Business.Models
+{
+    public enum Sexo
+    {
+        Masculino = 1,
+        Feminino = 2
+    }
+}
